4a7ec3453c48fe81617e0686c7eb1aafc2cbeeab
Eric <temporaldriver@outlook.com>
2023-05-18 06:00:42 -0400

    - cleaned up command_parser.py with args I'm not using yet, no reason keeping them in there

--- modules/command_parser.py
+++ modules/command_parser.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
 
-
-
 class CaseInsensitive:
     def __call__(self, value):
         return value.lower()
@@ -19,13 +17,6 @@ def validate_date_format(cmd_parse, date_string):
 def process_args(parser):
     required_choices = parser.add_argument_group('Required Arguments')
     required_choices.add_argument(
-        '-f', '--format',
-        type=CaseInsensitive(),
-        choices=['gif', 'mp4'],
-        required=True,
-        help='Choose the format to generate your timelapse (.GIF or .MP4)'
-    )
-    required_choices.add_argument(
         '-s', '--sat',
         type=CaseInsensitive(),
         choices=['east', 'west'],
@@ -39,14 +30,6 @@ def process_args(parser):
         required=True,
         help='Specify which region (Full Disk or Continental US)'
     )
-    # parser.add_argument(
-    #     '-b', '--band',
-    #     type=CaseInsensitive(),
-    #     default='geocolor',
-    #     choices=['airmass', 'daycloudphase', 'dust',
-    #              'firetemperature', 'geocolor', 'sandwich'],
-    #     help='Specify which band (GEOCOLOR is default)'
-    # )
     parser.add_argument(
         '--size',
         type=CaseInsensitive(),
